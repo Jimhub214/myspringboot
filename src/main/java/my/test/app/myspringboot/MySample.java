da4299f1929759a7fb9c22a1bea336a7d3e98d85
Jimmy <Jimmy@test.de>
2017-12-31 00:19:09 +0100

    init

---
+++ src/main/java/my/test/app/myspringboot/MySample.java
@@ -0,0 +1,14 @@
+package my.test.app.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySample.class, args);
+
+	}
+
+}
---
+++ src/main/java/my/test/app/myspringboot/prf/MyController.java
@@ -0,0 +1,15 @@
+package my.test.app.myspringboot.prf;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@RequestMapping(path = "/hallo", method = RequestMethod.GET)
+	public String sayOk() {
+		return "ok";
+	}
+
+}
